c568faa19fd884447892e946304bee69fbb361eb
cloudkim2025 <cloudviola@icloud.com>
2025-03-19 21:23:23 +0900

    오브젝트의 동일성과 동등성

--- src/main/java/com/example/objecttesk/test/UserDaoTest.java
+++ src/main/java/com/example/objecttesk/test/UserDaoTest.java
@@ -2,17 +2,19 @@ package com.example.objecttesk.test;
 
 import com.example.objecttesk.dao.DaoFactory;
 import com.example.objecttesk.dao.UserDao;
-import com.example.objecttesk.domain.User;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.sql.SQLException;
 
 public class UserDaoTest {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        UserDao dao = context.getBean("userDao", UserDao.class);
+    public static void main(String[] args) throws Exception {
+        // DaoFactory의 인스턴스를 직접 생성한 후, 매번 새로운 UserDao를 받아옴
+        DaoFactory factory = new DaoFactory();
+        UserDao dao1 = factory.userDao();
+        UserDao dao2 = factory.userDao();
 
+        System.out.println("dao1: " + dao1);
+        System.out.println("dao2: " + dao2);
     }
 }
